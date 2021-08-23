93717b9bf414fc1f1122878d2da5d5cfb208178e
gualtierigio <gualtierigio@gmail.com>
2021-08-23 17:26:51 +0200

    inizzializzazione esercizio dadi

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+
+
+let dadoMio = parseInt( (6 * Math.random() ) ) + 1;
+let dadoGiocatore = parseInt( (6 * Math.random() ) ) + 1;
+
+if (dadoGiocatore < dadoMio){
+    dadoMio = print("ho vinto");
+    dadoGiocatore = print("ho perso");
+}
+else (dadoGiocatore > dadoMio)
+    dadoMio = print("ho perso");
+    dadoGiocatore = print("ho vinto");
+}
+else if (dadoGiocatore = dadoMio) 
+    dadoMio = print("siamo pari");
+    dadoGiocatore = print("siamo pari");
+}
+
+console.log(dadoGiocatore)
