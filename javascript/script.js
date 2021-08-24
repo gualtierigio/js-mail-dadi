eb93b45f1a4f1ba383c6cc9941ab2253b40e3705
gualtierigio <gualtierigio@gmail.com>
2021-08-24 15:08:58 +0200

    e-mail non funziona

--- javascript/script.js
+++ javascript/script.js
@@ -4,14 +4,25 @@ let dadoMio = parseInt( (6 * Math.random() ) ) + 1;
 let dadoGiocatore = parseInt( (6 * Math.random() ) ) + 1;
 
 if (dadoGiocatore > dadoMio){
-    dadoGiocatore = "Ho vinto";
-    dadoMio = "ho perso";
+    dadoGiocatore = "vince il giocatore!";
+    dadoMio = "ho perso :,(";
 }else if(dadoMio > dadoGiocatore){
-    dadoMio = "ho vinto";
-    dadoGiocatore = "ho perso";
+    dadoMio = "vince il banco!";
+    dadoGiocatore = "hai perso :,(";
 }else {
     dadoMio = "siamo pari";
     dadoGiocatore = "siamo pari";
 }
 
 console.log(dadoGiocatore)
+console.log(dadoMio)
+
+let clientsMail = ["ciccio@bello.com", "gino@pasticcino.it", "mario@ttide.com", "seren@ata.net", "las@palmas.it"]
+
+document.getElementById("accesso").innerHTML
+
+if (accesso != clientsMail){
+    alert = "tu non puoi passare";
+}else {
+    print("sei il benvenuto");
+}
